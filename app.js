9e1cbae20960bb073d34e09fe84d4da31e5a36dd
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-09-09 11:07:47 -0300

    Testando uma nova forma de mostrar as imagens na tela; E falta ainda customização pelo css

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 //Declarando o botão que usarei para fazer uma ação de busca
 const botaoPesquisarRaca = document.getElementById('botao')
+const galeria = document.getElementById('imagens');
+
 
 //funcao de buscar imagem através de API. Async é necessário para utilização do AWAIT
 async function buscarImagens(){
@@ -22,12 +24,18 @@ async function buscarImagens(){
 async function mostrarImagens() {
     //Essa constante espera que a função buscarImagens retorne imagens.message para continuar a ação
     const urls = await buscarImagens(imagens.message);
-    const galeria = document.getElementById('imagens');
+    galeria.textContent = '';
 
-        galeria = urls //Aqui a constante declarada galeria vai definir o conteudo HTML
-          .map(url => `<img src="${url}" alt="Imagem da galeria">`) //
-          .join('');//
-      }
+    //Criando uma constante que vai receber um .map que vai buscar as url através de uma arrow fuction;
+    const elementos = urls.map(url => {
+        const img = document.createElement('img'); //Em seguida é declarado a criação do elemento img que vai receber o src e alt da imagem e return img.
+        img.src = url;
+        img.alt = 'Imagem da raça';
+        return img;
+      });
+
+      //
+      elementos.forEach(img => galeria.appendChild(img));
+    }
 
 botaoPesquisarRaca.addEventListener('click', mostrarImagens)
-
